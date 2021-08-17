a8cb3a1ef0998d4e240d485b22d2408388858eca
Klosiek <klosiek19791@gmail.com>
2021-08-17 19:35:36 +0200

    Add axios and search input logic

--- src/components/UsersList/UsersList.styles.tsx
+++ src/components/UsersList/UsersList.styles.tsx
@@ -1,1 +1,72 @@
 import styled from "styled-components";
+import { MEDIA_QUERIES } from "shared/mediaQueries";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Header = styled.h1``;
+
+export const Input = styled.input`
+  border-width: 2px;
+  border-color: #000;
+  width: 200px;
+  height: 20px;
+  :focus-visible {
+    outline: unset;
+  }
+`;
+
+export const ListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 4px;
+  height: 200px;
+  width: 360px;
+  white-space: nowrap;
+  margin-top: 24px;
+  overflow-y: auto;
+
+  ${MEDIA_QUERIES.sm} {
+    overflow-y: hidden;
+    height: unset;
+    white-space: pre-wrap;
+    width: 300px;
+  }
+`;
+
+export const List = styled.ol`
+  counter-reset: item;
+  margin-left: 0;
+  padding-left: 0;
+`;
+
+export const ListItem = styled.li`
+  margin-bottom: 5px;
+  display: flex;
+  align-items: center;
+  ::before {
+    content: counter(item) ". ";
+    counter-increment: item;
+    width: 24px;
+    opacity: 0.5;
+    font-size: 14px;
+  }
+`;
+
+export const UserContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const UserName = styled.div`
+  font-weight: 600;
+  margin-right: 8px;
+`;
+
+export const UserNickname = styled.div`
+  opacity: 0.4;
+  font-size: 14px;
+`;
--- src/components/UsersList/UsersList.tsx
+++ src/components/UsersList/UsersList.tsx
@@ -1,8 +1,56 @@
 import * as Types from "./UsersList.types";
 import * as Styles from "./UsersList.styles";
+import useAxios from "axios-hooks";
+import { API_ENDPOINTS, API_URL } from "shared/endpoints";
+import { useState } from "react";
+import { SyncLoader } from "react-spinners";
+import toast from "react-hot-toast";
 
-const UsersList = ({}: Types.Props) => {
-  return <></>;
+const UsersList = () => {
+  const [{ data, loading, error }] = useAxios<Types.User[]>(
+    `${API_URL}${API_ENDPOINTS.USERS}`
+  );
+  const [searchPhrase, setSearchPhrase] = useState("");
+
+  const searchByName = (user: Types.User) => {
+    const phrase = searchPhrase.toLocaleLowerCase();
+    return (
+      user.name.toLocaleLowerCase().includes(phrase) ||
+      user.username.toLocaleLowerCase().includes(phrase)
+    );
+  };
+
+  if (error) {
+    toast.error("Something went wrong, please try again later!");
+  }
+
+  return (
+    <Styles.Container>
+      <Styles.Header>Users list</Styles.Header>
+      <Styles.Input
+        data-testid="searchInput"
+        type="text"
+        placeholder="Search by user name..."
+        value={searchPhrase}
+        onChange={(e) => setSearchPhrase(e.currentTarget.value)}
+      ></Styles.Input>
+      <Styles.ListContainer>
+        {data && !loading && !error && (
+          <Styles.List data-testid="usersList">
+            {data.filter(searchByName).map((user) => (
+              <Styles.ListItem key={user.id}>
+                <Styles.UserContainer>
+                  <Styles.UserName>{user.name}</Styles.UserName>
+                  <Styles.UserNickname>{`@${user.username}`}</Styles.UserNickname>
+                </Styles.UserContainer>
+              </Styles.ListItem>
+            ))}
+          </Styles.List>
+        )}
+        <SyncLoader loading={loading} size={15} />
+      </Styles.ListContainer>
+    </Styles.Container>
+  );
 };
 
 export default UsersList;
