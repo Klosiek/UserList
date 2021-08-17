511790797331501a9411e6248d1d5e0c751da789
Klosiek <klosiek19791@gmail.com>
2021-08-17 15:26:51 +0200

    Add UsersList component

---
+++ src/components/UsersList/UsersList.tsx
@@ -0,0 +1,8 @@
+import * as Types from "./UsersList.types";
+import * as Styles from "./UsersList.styles";
+
+const UsersList = ({}: Types.Props) => {
+  return <></>;
+};
+
+export default UsersList;
